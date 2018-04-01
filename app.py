8b9452e0a15486037c53ea06d5ebd303717d7a82
“Victor <vivi510@Victors-MacBook-Pro.local>
2018-04-01 05:13:27 +0100

    changes

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, Response
 from datetime import datetime
+import tensorflow as tf
+import cv2
+import numpy as np
+
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
