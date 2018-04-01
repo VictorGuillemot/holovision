ce18192a1c669a1b13b45359de1f26fc4b7725e4
“Victor <vivi510@Victors-MacBook-Pro.local>
2018-04-01 19:34:36 +0100

    changes

--- app.py
+++ app.py
@@ -7,15 +7,9 @@ import numpy as np
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+def index():
+    return render_template('index.html')
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
